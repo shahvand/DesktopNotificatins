0c3e9eaab3441d99beb14789143bd5938228b6c7
Jalal Shahvand <J.Shahvand@Aftabnetad.com>
2018-12-22 10:07:40 +0330

    Add project files.

---
+++ Notification/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Notification
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.timerNotif = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "System tray demo";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(104, 48);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.showToolStripMenuItem.Text = "Show";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "011111";
+            this.notifyIcon1.BalloonTipTitle = "0111111111";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "01";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timerCheck_Tick);
+            // 
+            // timerNotif
+            // 
+            this.timerNotif.Enabled = true;
+            this.timerNotif.Interval = 10000;
+            this.timerNotif.Tick += new System.EventHandler(this.timerInterval_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 54);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        public System.Windows.Forms.Timer timer;
+        public System.Windows.Forms.Timer timerNotif;
+    }
+}
+
---
+++ Notification/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceProcess;
+using System.Diagnostics;
+namespace Notification
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //Process[] pname = Process.GetProcessesByName("Assistant");
+            //if (pname.Length > 0)
+            //{
+            //    foreach (var process in Process.GetProcessesByName("Assistant"))
+            //    {
+            //        process.Kill();
+            //    }
+            //    Process.Start(@"\\192.168.0.1\Software\Assistant\Assistant.exe");
+
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Process Not running");
+            //}
+
+            //this.WindowState = FormWindowState.Minimized;
+            //this.ShowInTaskbar = false;
+
+            //this.Hide();
+            ////Application.Exit();
+            //notifyIcon1.Visible = false;
+            //timerCheck.Interval = 1000;
+
+            //int timeinterval2 = Properties.Settings.Default.NotifIntervalEnabledCheck;
+            //timerCheck.Interval = timeinterval2;
+        }
+
+
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void timerCheck_Tick(object sender, EventArgs e)
+        {
+            int timeinterval =  Properties.Settings.Default.TimerInterval;
+            timer.Interval = timeinterval;
+
+
+            if (Properties.Settings.Default.NotifEnabled == false)
+
+           {
+                timerNotif.Enabled = false;
+
+            }
+            if (Properties.Settings.Default.NotifEnabled == true)
+            {
+                timerNotif.Enabled = true;
+                int timernotif = Properties.Settings.Default.NotifInterval;
+                timerNotif.Interval = timernotif;
+            }
+
+
+
+
+        }
+
+        private void timerInterval_Tick(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = true;
+            string NotifTitle = Properties.Settings.Default.NotifTitle;
+            string NotifDescription = Properties.Settings.Default.NotifMessage;
+            notifyIcon1.ShowBalloonTip(1000, NotifTitle, NotifDescription, ToolTipIcon.Info);
+            notifyIcon1.Visible = false;
+        }
+    }
+}
