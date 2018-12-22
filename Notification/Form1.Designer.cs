26ee6dfb5d5bbe04943fb5c8678a454bbad7ced5
jalal shahvand <shahvand@live.com>
2018-12-22 10:13:09 +0330

    Start Project

--- Notification/Form1.Designer.cs
+++ Notification/Form1.Designer.cs
@@ -44,7 +44,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(22, 12);
+            this.label1.Location = new System.Drawing.Point(23, 12);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(159, 24);
             this.label1.TabIndex = 0;
